64153502caa8a15dd902dca4b95ea04d2ea9be49
Cooper Maruyama <cooper@darkmatter.io>
2025-12-23 15:10:10 -0800

    wip: alpha

--- nix/packages/stackpanel-cli/default.nix
+++ nix/packages/stackpanel-cli/default.nix
@@ -7,15 +7,29 @@
   lib,
   ...
 }:
+let
+  stackpanelGoSrc = pkgs.callPackage ../stackpanel-go {};
+in
+let
+  repoRoot = ../../..;
+  compositeSrc = pkgs.runCommand "stackpanel-cli-src" { } ''
+    mkdir -p $out/apps/cli
+    mkdir -p $out/packages/stackpanel-go
+    cp -R ${repoRoot}/apps/cli/* $out/apps/cli/
+    cp -R ${repoRoot}/packages/stackpanel-go/* $out/packages/stackpanel-go/
+  '';
+in
 pkgs.buildGoModule {
   pname = "stackpanel-cli";
   version = "0.1.0";
 
-  src = ../../../apps/cli;
+  # Use a minimal source tree that contains both the CLI and the local module
+  src = compositeSrc;
+  modRoot = "apps/cli";
 
-  # This will need to be updated after first build
-  # Run: nix-prefetch-url --unpack <url> or use lib.fakeHash
-  vendorHash = null; # Uses vendored dependencies
+
+  # Nix will vendor deterministically from go.mod/go.sum
+  vendorHash = "sha256-orEq07AQsCXLLn7bLBZQMA+KXzz3NPuFt4Uh1O8KjaI=";
 
   ldflags = [
     "-s"
