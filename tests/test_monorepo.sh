0c323f2ed168b4cfe3c4190de139ca39be52ffee
Cooper Maruyama <me@cooperm.com>
2026-03-30 07:32:37 -0700

    add example

---
+++ tests/test_monorepo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# let's use the repo's alchemy version (but we can't use workspace:* so must use link)
+cd alchemy
+bun link
+# install deps and tsc -b
+cd ../nix/flake/templates/example-monorepo
+bun i
+bun run build
+# deploy backend and then frontend
+bun run deploy
+# sleep for a bit to ensure deployment is complete
+# destroy frontend and then backend
+bun run destroy
+# second destroy is to ensure that it still works even if `backend` has already been destroyed
+bun run destroy
